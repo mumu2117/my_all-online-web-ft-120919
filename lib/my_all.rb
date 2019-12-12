00708ae6ce621283c4c0e14a3f2535530c1027b6
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 13:10:06 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+counter=0
+end 	res=[]
+while counter < collection.size
+  res << yield(collection[counter])
+  counter+=1
+end
+if res.include?(false)
+  false
+else
+  true
+end
 end
